61d3cfa0856c5564c81cb16eab5ef6efd917fca5
Alex Wang <36569033@qq.com>
2022-07-07 20:39:01 +0800

    Compile WLAN/sound as module

---
+++ make_boot_img/copy_modules.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+cp ../out/drivers/staging/qcacld-3.0/wlan.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/
+cp ../out/techpack/audio/asoc/codecs/wcd-core.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/asoc/codecs/wcd934x/snd-soc-wcd934x.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/asoc/codecs/snd-soc-wcd9xxx.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound
+cp ../out/techpack/audio/soc/pinctrl-wcd.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/asoc/snd-soc-sdm845.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/ipc/wcd-dsp-glink.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
