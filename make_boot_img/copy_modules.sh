a78a41237f9e9fc3cfc535af4213227887066fe7
BirdZhang <0312birdzhang@gmail.com>
2024-09-19 11:09:36 +0800

    some update

--- make_boot_img/copy_modules.sh
+++ make_boot_img/copy_modules.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-cp ../out/drivers/staging/qcacld-3.0/wlan.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/
-cp ../out/techpack/audio/asoc/codecs/wcd-core.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
-cp ../out/techpack/audio/asoc/codecs/snd-soc-wcd9xxx.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound
-cp ../out/techpack/audio/soc/pinctrl-wcd.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
-cp ../out/techpack/audio/asoc/snd-soc-sdm845.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
-cp ../out/techpack/audio/ipc/wcd-dsp-glink.ko /mnt/disk1/agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/drivers/staging/qcacld-3.0/wlan.ko ../../../agnos-builder/userspace/usr/comma_pixel3/
+cp ../out/techpack/audio/asoc/codecs/wcd-core.ko ../../../agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/asoc/codecs/snd-soc-wcd9xxx.ko ../../../agnos-builder/userspace/usr/comma_pixel3/sound
+cp ../out/techpack/audio/soc/pinctrl-wcd.ko ../../../agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/asoc/snd-soc-sdm845.ko ../../../agnos-builder/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/ipc/wcd-dsp-glink.ko ../../../agnos-builder/userspace/usr/comma_pixel3/sound/
---
+++ make_boot_img/copy_modules_new.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+cp ../out/drivers/staging/qcacld-3.0/wlan.ko ../../../agosbbb/userspace/usr/comma_pixel3/
+cp ../out/techpack/audio/asoc/codecs/wcd-core.ko ../../../agosbbb/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/asoc/codecs/snd-soc-wcd9xxx.ko ../../../agosbbb/userspace/usr/comma_pixel3/sound
+cp ../out/techpack/audio/soc/pinctrl-wcd.ko ../../../agosbbb/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/asoc/snd-soc-sdm845.ko ../../../agosbbb/userspace/usr/comma_pixel3/sound/
+cp ../out/techpack/audio/ipc/wcd-dsp-glink.ko ../../../agosbbb/userspace/usr/comma_pixel3/sound/
